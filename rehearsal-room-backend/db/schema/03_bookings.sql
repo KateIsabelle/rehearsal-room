44c4a6c5a0d158725ce7530ed5eae0f0044c2846
KateIsabelle <kate.davis887@gmail.com>
2021-05-08 23:59:36 +0000

    fix some typos in bookings table

--- rehearsal-room-backend/db/schema/03_bookings.sql
+++ rehearsal-room-backend/db/schema/03_bookings.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE bookings(
     id SERIAL PRIMARY KEY NOT NULL,
-    user_id NOT NULL INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    space_id NOT NULL INTEGER REFERENCES spaces(id) ON DELETE CASCADE,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    space_id INTEGER NOT NULL REFERENCES spaces(id) ON DELETE CASCADE,
     start_time TIMESTAMP NOT NULL,
     end_time TIMESTAMP NOT NULL,
     num_hours INTEGER NOT NULL,
-    status VARCHAR(255) DEFAULT 'pending' --pending,confirmed,rejected
+    status VARCHAR(255) DEFAULT 'pending', --pending,confirmed,rejected
     usage_description TEXT,
     special_requests TEXT,
     used_before BOOLEAN
