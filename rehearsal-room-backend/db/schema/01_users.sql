73fe4ff622d72b95dd339d000825122ec1b41444
KateIsabelle <kate.davis887@gmail.com>
2021-05-08 22:31:32 +0000

    create users table

--- rehearsal-room-backend/db/schema/01_users.sql
+++ rehearsal-room-backend/db/schema/01_users.sql
@@ -3,6 +3,24 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    organization_name VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone INTEGER,
+    password VARCHAR(255) NOT NULL,
+    photo VARCHAR,
+    description TEXT,
+    is_host BOOLEAN
 );
+
+-- Table users {
+--   id integer [primary key]
+--   first_name varchar [not null]
+--   last_name varchar [not null]
+--   organisation_name varchar
+--   email varchar [unique, not null]
+--   phone int
+--   photo varchar
+--   description text
+--   is_host boolean
+  
+-- }
