a8353631d34b1a55d247a54af9b01319c56499a6
KateIsabelle <kate.davis887@gmail.com>
2021-05-09 00:25:52 +0000

    fix seeding db typos/inconsistencies

--- rehearsal-room-backend/db/schema/02_spaces.sql
+++ rehearsal-room-backend/db/schema/02_spaces.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS spaces CASCADE;
 
 CREATE TABLE spaces(
     id SERIAL PRIMARY KEY NOT NULL,
-    user_id INTEGER REFERENCES users(id),
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
     title VARCHAR(255) NOT NULL,
     description TEXT,
--- rehearsal-room-backend/db/seeds/03_bookings.sql
+++ rehearsal-room-backend/db/seeds/03_bookings.sql
@@ -6,7 +6,7 @@ VALUES
 --('2021-05-10 12:00:00'::timestamp, 'P'),
 --('2021-05-10 2:00:00'::timestamp, 'P'),
 TO_TIMESTAMP('05/10/2021 12:00:00', 'MM/DD/YYYY HH24:MI:SS'),
-TO_TIMESTAMP('05/10/2021 2:00:00', 'MM/DD/YYYY HH24:MI:SS')
+TO_TIMESTAMP('05/10/2021 2:00:00', 'MM/DD/YYYY HH24:MI:SS'),
 2,
 'confirmed',
 'Description of why I need the space: Working it up and down, back and forth. Maybe, just to play a little, we''ll put a little tree here. Here''s another little happy bush',
@@ -17,7 +17,7 @@ false
 (2,
 1,
 TO_TIMESTAMP('05/17/2021 12:00:00', 'MM/DD/YYYY HH24:MI:SS'),
-TO_TIMESTAMP('05/17/2021 2:00:00', 'MM/DD/YYYY HH24:MI:SS')
+TO_TIMESTAMP('05/17/2021 2:00:00', 'MM/DD/YYYY HH24:MI:SS'),
 2,
 'confirmed',
 'Description of why I need the space - second time: We''ll put a happy little sky in here. See there how easy that is. Let''s make some happy little clouds in our world.',
@@ -28,7 +28,7 @@ true
 (2,
 1,
 TO_TIMESTAMP('05/24/2021 12:00:00', 'MM/DD/YYYY HH24:MI:SS'),
-TO_TIMESTAMP('05/24/2021 2:00:00', 'MM/DD/YYYY HH24:MI:SS')
+TO_TIMESTAMP('05/24/2021 2:00:00', 'MM/DD/YYYY HH24:MI:SS'),
 2,
 'confirmed',
 'Description of why I need the space - third time',
@@ -39,7 +39,7 @@ true
 (2,
 2,
 TO_TIMESTAMP('07/01/2021 08:00:00', 'MM/DD/YYYY HH24:MI:SS'),
-TO_TIMESTAMP('07/01/2021 12:00:00', 'MM/DD/YYYY HH24:MI:SS')
+TO_TIMESTAMP('07/01/2021 12:00:00', 'MM/DD/YYYY HH24:MI:SS'),
 4,
 'pending',
 'Description of why I need the space',
@@ -50,7 +50,7 @@ false
 (3,
 3,
 TO_TIMESTAMP('07/02/2021 16:00:00', 'MM/DD/YYYY HH24:MI:SS'),
-TO_TIMESTAMP('07/02/2021 20:00:00', 'MM/DD/YYYY HH24:MI:SS')
+TO_TIMESTAMP('07/02/2021 20:00:00', 'MM/DD/YYYY HH24:MI:SS'),
 4,
 'pending',
 'Description of why I need the space',
