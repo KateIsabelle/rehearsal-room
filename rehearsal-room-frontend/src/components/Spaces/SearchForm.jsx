5fe29720233f0bf2590700ab1c0cb2ba652a372f
Adele Noronha <adele.c.noronha@gmail.com>
2021-05-10 22:42:00 +0000

    intitate and render basic form

---
+++ rehearsal-room-frontend/src/components/Spaces/SearchForm.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function SearchForm(props) {
+  return (
+    <tag>
+      <h1>Hi, I'm going to be complicated to make. Love, Search.</h1>
+
+      <form>
+        <label>
+          Keyword Search:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </tag>
+
+
+  )
+
+}
