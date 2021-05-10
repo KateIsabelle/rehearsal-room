b035b65567486d4317d8051532b71890ade5bef2
Paul Ladd <vlendle@gmail.com>
2021-05-10 17:00:02 +0000

    Add react router to packages and put basic router implementation into Application.jsx

--- rehearsal-room-frontend/src/components/Application.jsx
+++ rehearsal-room-frontend/src/components/Application.jsx
@@ -1,20 +1,43 @@
+// Modules
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// Components
+import Header from './Header'
+
+// Images
 import logo from '../../src/logo.svg';
+
+// CSS
 import './App.css';
+
+// Hooks
 import useApplicationData from '../hooks/useApplicationData'
 
-function App() {
+export default function App() {
   const {
     state,
     dispatch
   } = useApplicationData();
   const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
   ));
-  return (<div className="App" >
-  <h1> Users </h1>
 
-  <ul> {userList} </ul>
-  </div >
+  return (
+    <Router>
+      <div className="App" >
+        <Header />
+      </div >
+      <Route path="/">
+        <Home />
+      </Route>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <Link to="/vancouver">Vancouver</Link>
+}
