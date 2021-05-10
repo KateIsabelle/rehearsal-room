90e5a0720cf12791d230fc0026073252db423138
Paul Ladd <vlendle@gmail.com>
2021-05-10 22:23:17 +0000

    Installing and using storybook router to fix bug caused by using components outside of a Router

--- rehearsal-room-frontend/.storybook/preview.js
+++ rehearsal-room-frontend/.storybook/preview.js
@@ -1,3 +1,7 @@
+import { addDecorator } from '@storybook/react'
+import StoryRouter from 'storybook-react-router'
+
+addDecorator(StoryRouter())
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
