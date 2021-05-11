7e6fba8423c8b6a3e6eb42981cb05388e4a719f3
Paul Ladd <vlendle@gmail.com>
2021-05-11 18:37:10 +0000

    Added amenities list to Spaces component. Added toggleable amenities list to SearchForm.

--- rehearsal-room-frontend/src/components/SearchForm.jsx
+++ rehearsal-room-frontend/src/components/SearchForm.jsx
@@ -2,160 +2,23 @@ import React from "react";
 
 
 export default function SearchForm(props) {
+  const { register, onSubmit, handleSubmit, amenities, showAdvanced } = props;
+  const amenitiesList = Object.keys(amenities).map(key => {
+    return(
+      <li key={key}>
+        <label for={key}>{amenities[key]}</label>
+        <input type="checkbox" {...register(key)} />
+      </li>
+    );
+  })
   return (
-    <tag>
       <form>
-        <label>
-          Keyword Search:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Search" />
-        <input type="submit" value="Advanced Search" />
+        <label for="keyword">Keyword Search:</label>
+        <input type="text" {...register("keyword")} />
+        <label for="showAdvanced">Advanced Search</label>
+        <input type="checkbox" {...register("showAdvanced")} />
+        {showAdvanced && <ul>{amenitiesList}</ul>}
       </form>
-
-      <h1>Amenities</h1>
-
-      <form>
-        <label>
-          Wifi:
-          <input type="checkbox"/>
-        </label>
-      </form>
-
-      <form>
-        <label>
-          Sound Proofing:
-          <input type="checkbox"/>
-        </label>
-      </form>
-
-      <form>
-        <label>
-          Sprung Floor:
-          <input type="checkbox"/>
-        </label>
-      </form>
-
-      <form>
-        <label>
-          Kitchenette:
-          <input type="checkbox"/>
-        </label>
-      </form>
-
-      <form>
-        <label>
-          Mirrors:
-          <input type="checkbox"/>
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Sound System
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Bathroom <img src="https://www.flaticon.com/premium-icon/icons/svg/1739/1739040.svg" width="14px" height="14px"></img>
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Indoor Space
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Outdoor Space
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Bike Parking
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Street Parking
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Private Parking
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Piano
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Natural Light
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Air Conditioning
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : 10 ft plus Ceiling
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Private
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Semi-private
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Wheelchair Accessible
-        </label>
-      </form>
-
-      <form>
-        <label>
-          <input type="checkbox"/>
-          : Self-entry
-        </label>
-      </form>
-
-    </tag>
 
 
   )
--- rehearsal-room-frontend/src/components/Spaces/index.jsx
+++ rehearsal-room-frontend/src/components/Spaces/index.jsx
@@ -9,12 +9,15 @@ import { useForm } from 'react-hook-form';
 import SpaceList from './SpaceList'
 import SearchForm from '../SearchForm'
 
-export default function Spaces(props) {
-  const { spacesData } = props
+export default function Spaces() {
+  const { city } = useParams();
+  const { register, handleSubmit, watch } = useForm();
+  const onSubmit = data => console.log(data);
 
   //spaces = array of objects
-  const { city } = useParams();
   const [spaces, setSpaces] = useState([])
+  const [filteredSpaces, setFilteredSpaces] = useState([])
+  const [advancedToggle, setAdvancedToggle] = useState(false)
   useEffect(() => {
     axios({
       method: 'GET',
@@ -27,9 +30,43 @@ export default function Spaces(props) {
     .catch((err) => console.log("get city data ERROR", err));
   }, [city]);
 
+  watch((data, {name, type}) => {
+    name === "showAdvanced" && setAdvancedToggle(data.showAdvanced)
+    console.log(data, name, type)
+  })
+
+  const amenities = {
+    "wifi": "Wifi",
+    "sound_proofing": "Sound proofing",
+    "sprung_floor": "Sprung floor",
+    "kitchenette": "Kitchenette",
+    "mirrors": "Mirrors",
+    "sound_system": "Sound system",
+    "bathroom": "Bathroom",
+    "indoor_space": "Indoor space",
+    "outdoor_space": "Outdoor space",
+    "bike_parking": "Bike parking",
+    "street_parking": "Street parking",
+    "private_parking": "Private parking",
+    "piano": "Piano",
+    "natural_light": "Natural light",
+    "air_conditioning": "Air conditioning",
+    "ten_ft_plus_ceiling": "10 ft plus ceiling",
+    "private": "Private",
+    "semi_private": "Semi-private",
+    "wheelchair_accessible": "Wheelchair-accessible",
+    "self_entry": "Self-entry"
+  }
+
   return (
     <>
-      <SearchForm />
+      <SearchForm 
+        register={register}
+        onSubmit={onSubmit}
+        handleSubmit={handleSubmit}
+        showAdvanced={advancedToggle}
+        amenities={amenities}
+      />
       <SpaceList
       spaces={spaces}
       />
