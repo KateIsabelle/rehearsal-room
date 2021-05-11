d67b3ed1f5b42111321df73d08ac57f7544bb8c0
Paul Ladd <vlendle@gmail.com>
2021-05-11 19:21:44 +0000

    Add filtering by parameters

--- rehearsal-room-frontend/src/components/Spaces/index.jsx
+++ rehearsal-room-frontend/src/components/Spaces/index.jsx
@@ -10,6 +10,33 @@ import SpaceList from './SpaceList'
 import SearchForm from '../SearchForm'
 
 export default function Spaces() {
+  // This list of amenities is used in two places:
+  // 1. To help filter the search results by amenity
+  // 2. To build the list of toggleable buttons in the SearchForm component
+  const amenities = {
+    "wifi": "Wifi",
+    "sound_proofing": "Sound proofing",
+    "sprung_floor": "Sprung floor",
+    "kitchenette": "Kitchenette",
+    "mirrors": "Mirrors",
+    "sound_system": "Sound system",
+    "bathroom": "Bathroom",
+    "indoor_space": "Indoor space",
+    "outdoor_space": "Outdoor space",
+    "bike_parking": "Bike parking",
+    "street_parking": "Street parking",
+    "private_parking": "Private parking",
+    "piano": "Piano",
+    "natural_light": "Natural light",
+    "air_conditioning": "Air conditioning",
+    "ten_ft_plus_ceiling": "10 ft plus ceiling",
+    "private": "Private",
+    "semi_private": "Semi-private",
+    "wheelchair_accessible": "Wheelchair-accessible",
+    "self_entry": "Self-entry"
+  }
+
+
   const { city } = useParams();
   const { register, handleSubmit, watch } = useForm();
   const onSubmit = data => console.log(data);
@@ -47,36 +74,16 @@ export default function Spaces() {
         // Filter if the title or description don't match what's entered.
         return false;
       }
-
-
+      for (const key of Object.keys(amenities)) {
+        if(parameters[key] === true && space[key] === false) {
+          return false
+        }
+      }
       return true
     })
     console.log(filtered)
     return filtered;
   };
-
-  const amenities = {
-    "wifi": "Wifi",
-    "sound_proofing": "Sound proofing",
-    "sprung_floor": "Sprung floor",
-    "kitchenette": "Kitchenette",
-    "mirrors": "Mirrors",
-    "sound_system": "Sound system",
-    "bathroom": "Bathroom",
-    "indoor_space": "Indoor space",
-    "outdoor_space": "Outdoor space",
-    "bike_parking": "Bike parking",
-    "street_parking": "Street parking",
-    "private_parking": "Private parking",
-    "piano": "Piano",
-    "natural_light": "Natural light",
-    "air_conditioning": "Air conditioning",
-    "ten_ft_plus_ceiling": "10 ft plus ceiling",
-    "private": "Private",
-    "semi_private": "Semi-private",
-    "wheelchair_accessible": "Wheelchair-accessible",
-    "self_entry": "Self-entry"
-  }
 
   return (
     <>
