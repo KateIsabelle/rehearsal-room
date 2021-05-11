03e9c0fb2eabc6dd9fa727d0a01d4ed4851ea4a2
KateIsabelle <kate.davis887@gmail.com>
2021-05-11 00:50:32 +0000

    update variable names and console logs in Space component

--- rehearsal-room-frontend/src/components/Space/index.jsx
+++ rehearsal-room-frontend/src/components/Space/index.jsx
@@ -9,100 +9,104 @@ const requestButton = () => {
 }
 
 export default function Space(props) {
-  const [data, setData] = useState({})
+  const [spaceData, setSpaceData] = useState({})
   const { space_id } = useParams();
   useEffect(() => {
     axios({
       method: 'GET',
-      url: `/api/space/${space_id}`,
+      url: `/api/space/1`,
     })
-    .then(({ data }) => {
-      console.log("Data from space page:", data);
-      setData(data[0])
+    .then(({ spaceData }) => {
+      console.log("Data from space page:", spaceData);
+      setSpaceData(spaceData[0])
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log("ERROR", err));
   }, [space_id]);
 
-  const dataList = Object.keys(data).map(key => (
-    <li><strong>{key}</strong>: {`${data[key]}`}</li>
+  const dataList = Object.keys(spaceData).map(key => (
+    <li><strong>{key}</strong>: {`${spaceData[key]}`}</li>
   ))
   return (
 
     <article className="">
 
-      <h1>{data.title}</h1>
-      <p>{data.city}, {data.province}, {data.country}</p>
-      <img src={data.cover_photo_url} alt="property" width="600" height="400"></img>
-      <p>{data.description}</p>
-      <div>Price per day: ${data.price_per_day / 100}</div>
-      <div>Price per hour: ${data.price_per_hour / 100}</div>
-      {data.organization_name && <div>Affiliated organization: {data.organization_name}</div>}
-      <div>Contact: {data.first_name} {data.last_name}, {data.email}</div>
+      <h1>{spaceData.title}</h1>
+      <p>{spaceData.city}, {spaceData.province}, {spaceData.country}</p>
+      <img src={spaceData.cover_photo_url} alt="property" width="600" height="400"></img>
+      <p>{spaceData.description}</p>
+      <div>Price per day: ${spaceData.price_per_day / 100}</div>
+      <div>Price per hour: ${spaceData.price_per_hour / 100}</div>
+      {spaceData.organization_name && <div>Affiliated organization: {spaceData.organization_name}</div>}
+      <div>Contact: {spaceData.first_name} {spaceData.last_name}, {spaceData.email}</div>
       <button onClick={requestButton}>Make a request</button>
 
       <table>
-        <tr>
-          <th>Features</th>
-        </tr>
-        { props.wifi && <tr>
-          <td>Wifi</td>
-        </tr>}
-        { props.sound_proofing && <tr>
-          <td>Sound Proof</td>
-        </tr>}
-        { props.sprung_floor && <tr>
-          <td>Spring floor</td>
-        </tr>}
-        { props.mirrors && <tr>
-          <td>Mirrors</td>
-        </tr>}
-        { props.kitchenette && <tr>
-          <td>Kitchenette</td>
-        </tr>}
-        { props.sound_system && <tr>
-          <td>Sound System</td>
-        </tr>}
-        { props.bathroom && <tr>
-          <td>Bathroom</td>
-        </tr>}
-        { props.indoor_space && <tr>
-          <td>Indoor Space</td>
-        </tr>}
-        { props.outdoor_space && <tr>
-          <td>Outdoor Space</td>
-        </tr>}
-        { props.bike_parking && <tr>
-          <td>Bike Parking</td>
-        </tr>}
-        { props.street_parking && <tr>
-          <td>Street Parking</td>
-        </tr>}
-        { props.private_parking && <tr>
-          <td>Private Parking</td>
-        </tr>}
-        { props.piano && <tr>
-          <td>Piano</td>
-        </tr>}
-        { props.natural_light && <tr>
-          <td>Natural Light</td>
-        </tr>}
-        { props.air_conditioning && <tr>
-          <td>Air Conditioning</td>
-        </tr>}
-        { props.ten_ft_plus_ceiling && <tr>
-          <td>High Ceilings (over 10 feet)</td>
-        </tr>}
-        { props.private && <tr>
-          <td>Private</td>
-        </tr>}
-        { props.semi_private && <tr>
-          <td>Semi-private</td>
-        </tr>}
-        { props.wheelchair_accessible && <tr>
-          <td>Wheelchair Accessible</td>
-        </tr>}
+        <thead>
+          <tr>
+            <th>Features</th>
+          </tr>
+        </thead>
+        <tbody>
+          { spaceData.wifi && <tr>
+            <td>Wifi</td>
+          </tr>}
+          { spaceData.sound_proofing && <tr>
+            <td>Sound Proof</td>
+          </tr>}
+          { spaceData.sprung_floor && <tr>
+            <td>Spring floor</td>
+          </tr>}
+          { spaceData.mirrors && <tr>
+            <td>Mirrors</td>
+          </tr>}
+          { spaceData.kitchenette && <tr>
+            <td>Kitchenette</td>
+          </tr>}
+          { spaceData.sound_system && <tr>
+            <td>Sound System</td>
+          </tr>}
+          { spaceData.bathroom && <tr>
+            <td>Bathroom</td>
+          </tr>}
+          { spaceData.indoor_space && <tr>
+            <td>Indoor Space</td>
+          </tr>}
+          { spaceData.outdoor_space && <tr>
+            <td>Outdoor Space</td>
+          </tr>}
+          { spaceData.bike_parking && <tr>
+            <td>Bike Parking</td>
+          </tr>}
+          { spaceData.street_parking && <tr>
+            <td>Street Parking</td>
+          </tr>}
+          { spaceData.private_parking && <tr>
+            <td>Private Parking</td>
+          </tr>}
+          { spaceData.piano && <tr>
+            <td>Piano</td>
+          </tr>}
+          { spaceData.natural_light && <tr>
+            <td>Natural Light</td>
+          </tr>}
+          { spaceData.air_conditioning && <tr>
+            <td>Air Conditioning</td>
+          </tr>}
+          { spaceData.ten_ft_plus_ceiling && <tr>
+            <td>High Ceilings (over 10 feet)</td>
+          </tr>}
+          { spaceData.private && <tr>
+            <td>Private</td>
+          </tr>}
+          { spaceData.semi_private && <tr>
+            <td>Semi-private</td>
+          </tr>}
+          { spaceData.wheelchair_accessible && <tr>
+            <td>Wheelchair Accessible</td>
+          </tr>}
+          </tbody>
     </table>
-    <Map latitude={data.latitude} longitude={data.longitude}/>
+    <Map latitude={spaceData.latitude} longitude={spaceData.longitude}/>
 
           <h3>Data from axios request:</h3>
           <ul>
