52c78eecdd0802f8f59badd39dfff9b41dbe92fb
Paul Ladd <vlendle@gmail.com>
2021-05-11 16:49:49 +0000

    Moved space load/render logic from SpaceList to Spaces

--- rehearsal-room-frontend/src/components/Spaces/SpaceList.jsx
+++ rehearsal-room-frontend/src/components/Spaces/SpaceList.jsx
@@ -1,27 +1,9 @@
-import React, { useState, useEffect }from "react";
-import { useParams } from 'react-router-dom'
 import SpaceListItem from './SpaceListItem'
-import axios from 'axios'
-
 
 export default function SpaceList(props) {
-  //spaces = array of objects
-  const { city } = useParams();
-  const [spaces, setSpaces] = useState([])
-  useEffect(() => {
-    axios({
-      method: 'GET',
-      url: `/api/spaces/${city}`,
-    })
-    .then(({ data }) => {
-      console.log("get city data: ", data);
-      setSpaces(data)
-    })
-    .catch((err) => console.log("get city data ERROR", err));
-  }, [city]);
 
-  const list = spaces.map(s => 
-    <SpaceListItem 
+  const list = props.spaces.map(s =>
+    <SpaceListItem
       key={s.id}
       spaceId={s.id}
       title={s.title}
--- rehearsal-room-frontend/src/components/Spaces/index.jsx
+++ rehearsal-room-frontend/src/components/Spaces/index.jsx
@@ -1,19 +1,38 @@
 //Spaces is the main page
 
-import React from "react";
-import { Fragment } from "react";
+import axios from 'axios';
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+// Components
 import SpaceList from './SpaceList'
 import SearchForm from '../SearchForm'
 
 export default function Spaces(props) {
   const { spacesData } = props
 
+  //spaces = array of objects
+  const { city } = useParams();
+  const [spaces, setSpaces] = useState([])
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: `/api/spaces/${city}`,
+    })
+    .then(({ data }) => {
+      console.log("get city data: ", data);
+      setSpaces(data)
+    })
+    .catch((err) => console.log("get city data ERROR", err));
+  }, [city]);
+
   return (
-    <Fragment>
+    <>
       <SearchForm />
       <SpaceList
-      spaces={spacesData}
+      spaces={spaces}
       />
-    </Fragment>
+    </>
   )
 }
