13e773126c74d1227f728f0941393cc22e2de915
Paul Ladd <vlendle@gmail.com>
2021-05-12 21:10:37 +0000

    Adding dummy components and basic layout for dashboard

--- rehearsal-room-frontend/src/components/Dashboard/index.jsx
+++ rehearsal-room-frontend/src/components/Dashboard/index.jsx
@@ -1,9 +1,146 @@
-import { palette } from '@material-ui/system';
-import { Box } from '@material-ui/core/Box';
+import { useEffect, useState } from 'react';
 
+// Material UI Components
+import { palette } from '@material-ui/system';
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Avatar';
+
+//AXIOS REQ GOES HERE
+const dummyBookings = [
+  {
+    id: 1,
+    user_id: 2,
+    requester_name: "Plimbus Perkins",
+    space_name: "The Big Cahoozey",
+    space_id: 1,
+    start_time: "05/10/2021 12:00:00",
+    end_time: "05/10/2021 2:00:00",
+    status: "pending",
+    usage_description: "i'm gonna wreck it",
+    used_before: true,
+  },
+  {
+    id: 2,
+    user_id: 2,
+    requester_name: "Plimbus Perkins",
+    space_name: "The Rat Maze",
+    space_id: 1,
+    start_time: "05/10/2021 12:00:00",
+    end_time: "05/10/2021 2:00:00",
+    status: "rejected",
+    usage_description: "i'm gonna wreck it",
+    used_before: true,
+  },
+  {
+    id: 3,
+    user_id: 2,
+    requester_name: "Plimbus Perkins",
+    space_id: 3,
+    space_name: "The Flop Garden",
+    start_time: "05/10/2021 12:00:00",
+    end_time: "05/10/2021 2:00:00",
+    status: "confirmed",
+    usage_description: "i'm gonna wreck it",
+    used_before: true,
+  },
+  {
+    id: 4,
+    user_id: 2,
+    requester_name: "Jango Mango",
+    space_name: "The Flop Garden",
+    space_id: 2,
+    start_time: "05/10/2021 12:00:00",
+    end_time: "05/10/2021 2:00:00",
+    status: "confirmed",
+    usage_description: "i'm gonna wreck it",
+    used_before: true,
+  }
+]
 
 export default function Dashboard(props) {
+  const { user } = props;
+  const [bookings, setBookings] = useState([])
+  useEffect(() => {
+    setBookings(dummyBookings)
+  })
+
   return (
-    <Box
+    <Container maxWidth="lg">
+      <Grid 
+        container
+        direction="row"
+        justify="flex-start"
+        align-items="center"
+        spacing={2}
+      >
+        <Grid item xs={3}>
+          <Paper bgcolor={palette.main} color="primary.contrastText">{user.first_name} {user.last_name}
+            <img src={user.photo} width="90%" alt="profile"/>
+            <p>{user.description}</p>
+            <p><strong>{user.organization_name}</strong></p>
+          </Paper>
+        </Grid>
+        <Grid
+          container
+          direction="column"
+          justify="flex-start"
+          xs={9}
+          spacing={2}>
+          <Grid item>
+            <Paper>My Spaces
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper>
+              Pending Booking Requests
+              <BookingList 
+                bookings={bookings}
+                bookingType="pending"
+              />
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper>
+              Confirmed Bookings
+              <BookingList
+                bookings={bookings}
+                bookingType="confirmed"
+              />
+            </Paper>
+          </Grid>
+        </Grid>
+
+
+      </Grid>
+    </Container>
+  )
+}
+
+function BookingList(props) {
+  const { bookings, bookingType } = props;
+
+  const bookingList =
+    bookings
+    .filter(booking => booking.status === bookingType)
+    .map(booking => (
+      <BookingListItem {...booking} />
+    ))
+  return (
+    <ul>
+      {bookingList}
+    </ul>
+  )
+}
+
+function BookingListItem(props) {
+  return (
+    <li>
+      <p><strong>Requester:</strong>{props.requester_name}</p> |
+    For: {props.space_name} |
+    Start time: {props.start_time} |
+    End time: {props.end_time}</li>
   )
 }
--- rehearsal-room-frontend/src/stories/Dashboard.stories.js
+++ rehearsal-room-frontend/src/stories/Dashboard.stories.js
@@ -3,6 +3,15 @@ import Dashboard from '../components/Dashboard'
 const Template = (args) => <Dashboard {...args} />
 
 export const Default = Template.bind({})
+Default.args = {
+  user: {
+    photo: "https://pbs.twimg.com/media/Eu_xsFsUUAUP0SF?format=jpg&name=4096x4096",
+    first_name: "Paul",
+    last_name: "Ladd",
+    description: "I'm Paul, and I'm a complete and utter goobus. Graphic design is my passion",
+    organization_name: "Organization Name Here"
+  }
+}
 
 export default {
   title:'Components/Dashboard',
