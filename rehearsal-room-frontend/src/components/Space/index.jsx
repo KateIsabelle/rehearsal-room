9d7fd29b7f815c86fa5e7812c2a583c6f0a112e6
KateIsabelle <kate.davis887@gmail.com>
2021-05-12 21:18:59 +0000

    rename features to amenities; remove old features table

--- rehearsal-room-frontend/src/components/Space/index.jsx
+++ rehearsal-room-frontend/src/components/Space/index.jsx
@@ -5,6 +5,8 @@ import axios from 'axios'
 import Map from './Map'
 import RentalRequest from '../RentalRequest/index'
 import { Button } from '../Button/Button'
+import AmenitiesList from './AmenitiesList'
+
 
 const requestButton = () => {
   alert("Request")
@@ -44,73 +46,9 @@ export default function Space(props) {
       <div>Contact: {spaceData.first_name} {spaceData.last_name}, {spaceData.email}</div>
       <Button size="small" label="Make a Request" onClick={() => setVisualMode("REQUEST_FORM")}/>
 
+      <h3>Features:</h3>
+      <AmenitiesList spaceData={spaceData}/>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Features</th>
-          </tr>
-        </thead>
-        <tbody>
-          { spaceData.wifi && <tr>
-            <td>Wifi</td>
-          </tr>}
-          { spaceData.sound_proofing && <tr>
-            <td>Sound Proof</td>
-          </tr>}
-          { spaceData.sprung_floor && <tr>
-            <td>Spring floor</td>
-          </tr>}
-          { spaceData.mirrors && <tr>
-            <td>Mirrors</td>
-          </tr>}
-          { spaceData.kitchenette && <tr>
-            <td>Kitchenette</td>
-          </tr>}
-          { spaceData.sound_system && <tr>
-            <td>Sound System</td>
-          </tr>}
-          { spaceData.bathroom && <tr>
-            <td>Bathroom</td>
-          </tr>}
-          { spaceData.indoor_space && <tr>
-            <td>Indoor Space</td>
-          </tr>}
-          { spaceData.outdoor_space && <tr>
-            <td>Outdoor Space</td>
-          </tr>}
-          { spaceData.bike_parking && <tr>
-            <td>Bike Parking</td>
-          </tr>}
-          { spaceData.street_parking && <tr>
-            <td>Street Parking</td>
-          </tr>}
-          { spaceData.private_parking && <tr>
-            <td>Private Parking</td>
-          </tr>}
-          { spaceData.piano && <tr>
-            <td>Piano</td>
-          </tr>}
-          { spaceData.natural_light && <tr>
-            <td>Natural Light</td>
-          </tr>}
-          { spaceData.air_conditioning && <tr>
-            <td>Air Conditioning</td>
-          </tr>}
-          { spaceData.ten_ft_plus_ceiling && <tr>
-            <td>High Ceilings (over 10 feet)</td>
-          </tr>}
-          { spaceData.private && <tr>
-            <td>Private</td>
-          </tr>}
-          { spaceData.semi_private && <tr>
-            <td>Semi-private</td>
-          </tr>}
-          { spaceData.wheelchair_accessible && <tr>
-            <td>Wheelchair Accessible</td>
-          </tr>}
-          </tbody>
-      </table>
       <Map latitude={spaceData.latitude} longitude={spaceData.longitude}/>
 
           <h3>Data from axios request:</h3>
