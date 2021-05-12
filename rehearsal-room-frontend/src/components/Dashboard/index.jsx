6e21d84551ee82a96a0395faa11697d7187f8a55
Paul Ladd <vlendle@gmail.com>
2021-05-12 18:23:23 +0000

    Adding initial Dashboard component/storry

---
+++ rehearsal-room-frontend/src/components/Dashboard/index.jsx
@@ -0,0 +1,9 @@
+import { palette } from '@material-ui/system';
+import { Box } from '@material-ui/core/Box';
+
+
+export default function Dashboard(props) {
+  return (
+    <Box
+  )
+}
---
+++ rehearsal-room-frontend/src/stories/Dashboard.stories.js
@@ -0,0 +1,10 @@
+import Dashboard from '../components/Dashboard'
+
+const Template = (args) => <Dashboard {...args} />
+
+export const Default = Template.bind({})
+
+export default {
+  title:'Components/Dashboard',
+  component: Dashboard
+}
