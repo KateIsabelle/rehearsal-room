ab36c59f3baa6c2982b15115a14974662f638e3d
KateIsabelle <kate.davis887@gmail.com>
2021-05-12 00:48:41 +0000

    set state for email input; take in setUserInfo props for onClick event

--- rehearsal-room-frontend/src/components/Login.jsx
+++ rehearsal-room-frontend/src/components/Login.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
-export default function Login({setBookings}) {
+export default function Login({setUserInfo}) {
+  const [email, setEmail] = useState("");
   
+
   return (
     <form>
       <label>
         <p>Username</p>
-        <input type="text" id="email"/>
+        <input type="text" id="email" onChange={setEmail}/>
       </label>
       <label>
         <p>Password</p>
         <input type="password" />
       </label>
       <div>
-        <button type="submit" onClick={()=> setBookings(2)}>Submit</button>
+        <button onClick={() => setUserInfo(email)}>Submit</button>
       </div>
     </form>
   
