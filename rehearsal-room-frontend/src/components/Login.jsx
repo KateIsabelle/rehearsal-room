923f3da855faacf6260f6ff5248833056efef24f
KateIsabelle <kate.davis887@gmail.com>
2021-05-13 00:15:16 +0000

    organize Sass files for compiling

--- rehearsal-room-frontend/src/components/Login.jsx
+++ rehearsal-room-frontend/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 import {Button} from '../components/Button/Button'
-import './Login.scss'
+// import './Login.scss'
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
