57d8295cc21f32cae24aecdfc3108d890d915ea8
Paul Ladd <vlendle@gmail.com>
2021-05-13 02:13:45 +0000

    Adding documentation to useReadableTimes

--- rehearsal-room-frontend/src/hooks/useReadableTimes.js
+++ rehearsal-room-frontend/src/hooks/useReadableTimes.js
@@ -1,3 +1,6 @@
+// Give this hook a start timestamp and an end timestamp
+// and it returns the date, the start time, and the end time.
+
 export default function useReadableTimes (start, end) {
   const timestampToDate = (timestamp) => {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
