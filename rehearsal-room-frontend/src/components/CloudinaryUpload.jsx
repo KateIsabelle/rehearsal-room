ed3d1e0f278ef80cf453e72f701233609d9adfd8
KateIsabelle <kate.davis887@gmail.com>
2021-05-14 13:18:45 +0000

    add pseudocode to CloudinaryUpload component functions

--- rehearsal-room-frontend/src/components/CloudinaryUpload.jsx
+++ rehearsal-room-frontend/src/components/CloudinaryUpload.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import axios from 'axios'
 
 export default function CloudinaryUpload() {
-  const [fileInput, setFileInput] = useState("")
-  const [selectedFile, setSelectedFile] = useState("")
-  //previewSource is a string that represents the image
+  //previewSource is a base-64 encoded string that represents the image 
   const [previewSource, setPreviewSource] = useState()
 
+  //sets previewSource when file chosen
   const previewFile = file => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
@@ -14,23 +13,24 @@ export default function CloudinaryUpload() {
       setPreviewSource(reader.result)
     }
   }
-
-  const handleSubmitFile = e => {
-    console.log("submitting")
-    e.preventDefault();
-    if(!previewSource) return;
-    uploadImage(previewSource);
-
-  }
-
-  const uploadImage = base64EncodedImage => {
-    axios.post('/api/upload', {data: base64EncodedImage})
-  }
-
+  //calls previewFile with file chosen
   const handleFileInputChange = e => {
     const file = e.target.files[0] // just takes one file
     previewFile(file)
   }
+  //makes post request to backend
+  const uploadImage = base64EncodedImage => {
+    axios.post('/api/upload', {data: base64EncodedImage})
+  }
+  //on submit, calls uploadImage with previewSource
+  const handleSubmitFile = e => {
+    console.log("submitting")
+    e.preventDefault();
+    if(!previewSource) return; 
+    uploadImage(previewSource);
+  }
+
+
 
   return (
     <div className="cloudinary-upload">
@@ -40,12 +40,11 @@ export default function CloudinaryUpload() {
           type="file" 
           name="image" 
           onChange={handleFileInputChange} 
-          value={fileInput} 
           className=""
         />    
         <button className="" type="submit">Submit</button>  
       </form>
-      {previewSource && <img src={previewSource} alt="" style={{height: '300px'}}/>}
+      {previewSource && <img src={previewSource} alt="" style={{height: '100px'}}/>}
     </div>
   )
 }
