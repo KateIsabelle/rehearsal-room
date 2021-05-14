c8a445392dfa43641dd5563760ae361c0f5d1ec8
KateIsabelle <kate.davis887@gmail.com>
2021-05-14 01:54:16 +0000

    write cloudinary upload post router

--- rehearsal-room-backend/routes/upload.js
+++ rehearsal-room-backend/routes/upload.js
@@ -2,21 +2,41 @@ const express = require('express');
 const { route } = require('.');
 const router = express.Router();
 
+const {cloudinary} = require('../utils/cloudinary')
+
 
 module.exports = ({
   
 }) => {
-  // GET space by id
-  // router.get('/:space_id', (req, res) => {
-  //   const spaceId = req.params.space_id
-  //   getSpaceById(spaceId)
-  //     .then(space => res.json(space))
-  //     .catch(err => res.json({error: err.message}));
-  // });
-
+  //POST uploads photo to cloudinary
   router.post('/', (req, res) => {
-    
+    const fileStr = req.body.data
+    cloudinary.uploader.upload(fileStr, { upload_preset: 'rehearsal_room' })
+    .then(res => {
+      console.log("cloudinary response**", res)
+      console.log("reponse.public_id", res.public_id)
+    })
+      .catch(err => {
+      console.log(err)
+      res.status(500).json({err:'Something went wrong***'})
+    })
   })
 
   return router;
 }
+
+//route with async/await from tutorial:
+// router.post('/', async (req, res) => {
+//   try {
+//     const fileStr = req.body.data
+//     const uploadedResponse = await cloudinary.uploader.upload(fileStr, { upload_preset: 'rehearsal_room' })
+//     console.log(uploadedResponse)
+//     console.log("public key", uploadedResponse.public_key)
+//     res.json({msg: "yay upload"})
+//   } catch(err) {
+//     console.log(err)
+//     res.status(500).json({err:'Something went wrong***'})
+//   }
+// })
+
+
