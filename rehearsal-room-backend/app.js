441ca0f06acbff715a7f2b6c796ba5a52325e5d2
KateIsabelle <kate.davis887@gmail.com>
2021-05-14 00:42:20 +0000

    adjust limit in express.json & express.urlencoded for taking in image files

--- rehearsal-room-backend/app.js
+++ rehearsal-room-backend/app.js
@@ -20,8 +20,9 @@ var app = express();
 
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//set limit higher for uploading images:
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //here we call the function created, with a dbHelpers parameter
