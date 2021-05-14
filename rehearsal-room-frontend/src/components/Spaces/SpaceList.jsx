57dd3bbeb5233e3c58bc97e06b30cd391495acc1
KateIsabelle <kate.davis887@gmail.com>
2021-05-14 15:04:20 +0000

    update props for SpaceListItem

--- rehearsal-room-frontend/src/components/Spaces/SpaceList.jsx
+++ rehearsal-room-frontend/src/components/Spaces/SpaceList.jsx
@@ -9,10 +9,7 @@ export default function SpaceList(props) {
   const list = props.spaces && props.spaces.map(s =>
     <SpaceListItem
       key={s.id}
-      spaceId={s.id}
-      title={s.title}
-      description = {s.description}
-      photoUrl={s.thumbnail_photo_url}
+      {...s}
     />
     )
 
