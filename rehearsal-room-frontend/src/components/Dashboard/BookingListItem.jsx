53b05390e6282b23b4b8e8ea7c03ad39f3c790c2
Paul Ladd <vlendle@gmail.com>
2021-05-15 23:31:37 +0000

    Adding more booking list item styles

--- rehearsal-room-frontend/src/components/Dashboard/BookingListItem.jsx
+++ rehearsal-room-frontend/src/components/Dashboard/BookingListItem.jsx
@@ -19,31 +19,7 @@ export default function BookingListItem(props) {
 
   const unselectedBooking = (
     <>
-      { host &&
-        <div className="booking-list-item-section">
-          <h4 className="booking-list-item-section-header">From</h4>
-          <p></p>
-        </div>
-      }
-      { !host &&
-        <div className="booking-list-item-section">
-          <h4 className="booking-list-item-section-header">Status</h4>
-          <p><strong>{upcase(props.status)}</strong></p>
-        </div>
-      }
-        <div className="booking-list-item-section">
-          <h4 className="booking-list-item-section-header">Space</h4>
-          <p>{props.space_name}</p>
-        </div>
-      <div className="booking-list-item-section">
-        <h4 className="booking-list-item-section-header">Usage Description</h4>
-        <p>{shortDesc}</p>
-      </div>
-      <div className="booking-list-item-section">
-        <h4 className="booking-list-item-section-header">Date</h4>
-        <p>{date}</p>
-        <p>{props.start_time} to {props.end_time}</p>
-      </div>
+      
     </>
   )
 
@@ -52,39 +28,52 @@ export default function BookingListItem(props) {
       className={"booking-list-item " + props.status}
       onClick={() => handlers.select(props.id)}
     >
-    { !selected &&
-      unselectedBooking
-    }
-    {/* If this component is not the currently-selected component,
-        we show a shortened listing with no buttons. */}
-    { selected && host &&
-      <>
-      <strong> From: </strong>{props.requester_name} |
-      <strong> Space: </strong>{props.space_name} |
-      <strong> Description: </strong>{props.usage_description} |
-      <strong> Time: </strong>{date} from {props.start_time} to {props.end_time}
-      {props.status === "pending" &&
-        <>
-        <Button onClick={() => handlers.confirm(props.id)} label="Confirm"></Button>
-        <Button onClick={() => handlers.reject(props.id)} label="Reject"></Button>
-        </>
+      <div className="booking-list-item-top">
+        { host &&
+          <div className="booking-list-item-section">
+            <h4 className="booking-list-item-section-header">From</h4>
+            <p></p>
+          </div>
+        }
+        { !host &&
+          <div className="booking-list-item-section">
+            <h4 className="booking-list-item-section-header">Status</h4>
+            <p><strong>{upcase(props.status)}</strong></p>
+          </div>
+        }
+          <div className="booking-list-item-section">
+            <h4 className="booking-list-item-section-header">Space</h4>
+            <p>{props.space_name}</p>
+          </div>
+        <div className="booking-list-item-section">
+          <h4 className="booking-list-item-section-header">Usage Description</h4>
+          <p>{selected ? props.usage_description : shortDesc}</p>
+        </div>
+        <div className="booking-list-item-section">
+          <h4 className="booking-list-item-section-header">Date</h4>
+          <p>{date}</p>
+          <p>{props.start_time} to {props.end_time}</p>
+        </div>
+      </div>
+      { selected &&
+      <div className="booking-list-item-expansion">
+        {props.status === "pending" && host &&
+          <>
+          <Button onClick={() => handlers.confirm(props.id)} label="Confirm"></Button>
+          <Button onClick={() => handlers.reject(props.id)} label="Reject"></Button>
+          </>
+        }
+        {props.status === "confirmed" && host &&
+          <Button onClick={() => handlers.cancel(props.id)} label="Cancel"></Button>
+        }
+        { !host &&
+          <Button
+            onClick={() => handlers.cancel(props.id)}
+            label={props.status === "rejected" ? "Delete" : "Cancel" }
+          />
+        }
+      </div>
       }
-      {props.status === "confirmed" &&
-        <Button onClick={() => handlers.cancel(props.id)} label="Cancel"></Button>
-      }
-      </>
-    }
-    { selected && !host && <>
-      <strong> Space: </strong>{props.space_name} |
-      <strong> Description: </strong>{props.usage_description} |
-      <strong> Time: </strong>{date} from {props.start_time} to {props.end_time} |
-      <strong> Status: </strong>{upcase(props.status)}
-      <Button
-        onClick={() => handlers.cancel(props.id)}
-        label={props.status === "rejected" ? "Delete" : "Cancel" }
-      ></Button>
-      </>
-    }
     </div>
   )
 }
