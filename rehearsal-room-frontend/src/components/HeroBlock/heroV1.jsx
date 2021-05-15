084f4200b4ebd3f515ac88c6aa94895de59bf366
Adele Noronha <adele.c.noronha@gmail.com>
2021-05-15 03:05:44 +0000

    add images to hero block

--- rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
+++ rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
@@ -1,28 +1,48 @@
 import React from "react";
-import { Image, Transformation } from "cloudinary-react";
 import './_Hero.scss'
 
 export default function HeroV1(props) {
   return (
     <tag>
       <div id="container">
-        <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/mostafa-meraji-Jby30ALhnms-unsplash_olrgbs.jpg"/>
+      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_south/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/shutterstock_1563093277_1_lmcnph.jpg"/>
       </div>
 
     </tag>
   )
 
 }
+
 
 //reminder: AN has aspirations to build 3 Hero versions that roate
 export function HeroV2(props) {
   return (
     <tag>
       <div id="container">
-        <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/elijah-m-henderson-xgT3iQDIijU-unsplash_lmqcd5.jpg"/>
+      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/mostafa-meraji-Jby30ALhnms-unsplash_olrgbs.jpg"/>
       </div>
 
     </tag>
   )
 
 }
+
+export function HeroV3(props) {
+return (
+  <tag>
+    <div id="container">
+      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/shutterstock_786636211_1_ewbszy.jpg"/>
+
+      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/mostafa-meraji-Jby30ALhnms-unsplash_olrgbs.jpg"/>
+
+      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/elijah-m-henderson-xgT3iQDIijU-unsplash_lmqcd5.jpg"/>
+
+      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_south/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/a_0/v1621046537/shutterstock_1563093277_1_ffah0u.jpg"/>
+    </div>
+
+  </tag>
+)
+
+}
+
+
