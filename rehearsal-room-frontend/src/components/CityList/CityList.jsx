2f2d2109a513e486d7d81026c03f1f25116ac402
Adele Noronha <adele.c.noronha@gmail.com>
2021-05-15 00:54:41 +0000

    add cityList stylings with associate adjustments

---
+++ rehearsal-room-frontend/src/components/CityList/CityList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './_CityList.scss'
+
+export default function CityList() {
+  return (
+    <>
+      <div className="container">
+        <h1>Pick an area to explore </h1>
+        <div className="city-list">
+          <Link to="/spaces/vancouver" style={{textDecoration: 'none'}}className="city-item">Vancouver</Link>
+          <Link to="/spaces/vancouver" style={{textDecoration: 'none'}}className="city-item">Toronto</Link>
+          <Link to="/spaces/vancouver" style={{textDecoration: 'none'}}className="city-item">Montreal</Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
