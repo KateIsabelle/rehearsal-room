5439f7666d3d5e8c4f0f3ef71ca7de9f0b9e3b6c
Adele Noronha <adele.c.noronha@gmail.com>
2021-05-17 12:31:25 +0000

    style price and title

--- rehearsal-room-frontend/src/components/Spaces/SpaceListItem.jsx
+++ rehearsal-room-frontend/src/components/Spaces/SpaceListItem.jsx
@@ -9,27 +9,28 @@ const picStyle = {
 
 export default function SpaceListItem(props) {
   const { id, thumbnail_photo_url, title, price_per_hour, dashboard, onDeleteClick } = props
-  // console.log('SPACE LIST ITEM PROPS:',props)
   return (
-    <div className="item" onClick={() => console.log("clicked on space", id, title)}>
-       
-
-      <div className="text">
-        <h1>{title}</h1>
-        <h2>${price_per_hour / 100}/hr</h2>
+    <div className="sl-wrapper">
+      <div className="sl-item" onClick={() => console.log("clicked on space", id, title)}>
+        <div className="sl-title">
+          <p>{title}</p>
+        </div>
+        <Link className="sl-pict" to={"/space/" + id}>
+          <div>
+            <img src={thumbnail_photo_url} style={picStyle} alt=""/>
+            </div>
+        </Link>
       </div>
-      <Link className="pict" to={"/space/" + id}>
-        <div>
-          <img src={thumbnail_photo_url} style={picStyle} alt=""/>
-          </div>
-      </Link>
+      <div className="sl-pricebox">
+        <h2 className="sl-price">${price_per_hour / 100}/hr</h2>
+      </div>
       {dashboard &&
-        <Button 
-          label="Unlist"
-          danger="true"
-          onClick={() => onDeleteClick(id, title)}
-        />
-      }
+          <Button 
+            label="Unlist"
+            danger="true"
+            onClick={() => onDeleteClick(id, title)}
+          />
+        }
     </div>
   )
 }
