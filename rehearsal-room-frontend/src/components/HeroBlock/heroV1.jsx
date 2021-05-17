aa0ae6c6f5f000525243af2b07fc282f0e3b69d5
Adele Noronha <adele.c.noronha@gmail.com>
2021-05-17 06:08:06 +0000

    add banner choice

--- rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
+++ rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
@@ -14,7 +14,13 @@ export default function HeroV1(props) {
   return (
     <tag>
       <div id="container">
-      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/mostafa-meraji-Jby30ALhnms-unsplash_olrgbs.jpg"/>
+      {/* <img id="image" sizes="100vw" src="
+      https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1621230600/dance_mirror.jpg"
+      /> */}
+      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/dramatic_dude.jpg" />
+
+      {/* <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1621231127/fingers_piano.jpg" /> */}
+
       </div>
 
     </tag>
@@ -36,22 +42,6 @@ export function HeroV2(props) {
 
 }
 
-export function HeroV3(props) {
-return (
-  <tag>
-    <div id="container">
-      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/shutterstock_786636211_1_ewbszy.jpg"/>
 
-      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/mostafa-meraji-Jby30ALhnms-unsplash_olrgbs.jpg"/>
-
-      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/elijah-m-henderson-xgT3iQDIijU-unsplash_lmqcd5.jpg"/>
-
-      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_south/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/a_0/v1621046537/shutterstock_1563093277_1_ffah0u.jpg"/>
-    </div>
-
-  </tag>
-)
-
-}
 
 
