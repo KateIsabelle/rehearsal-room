4e48a6ede2d8319559868a0152a6e39fdf7031b0
KateIsabelle <kate.davis887@gmail.com>
2021-05-17 13:17:45 +0000

    clean up imports in Space/index

--- rehearsal-room-frontend/src/components/Space/index.jsx
+++ rehearsal-room-frontend/src/components/Space/index.jsx
@@ -1,12 +1,9 @@
-import React, { useEffect, useState, Fragment } from "react";
-import { useParams, useHistory } from 'react-router-dom';
-import axios from 'axios'
+import React from "react";
 
 import useSpaceData from '../../hooks/useSpaceData'
 
 import RentalRequest from '../RentalRequest/index'
 import Space_Show from './Space_Show'
-
 
 export default function Space(props) {
   const { 
@@ -16,25 +13,23 @@ export default function Space(props) {
     reroute
    } = useSpaceData();
 
-
   return (
 
     <article className="space-container">
-    { state.visualMode === "SPACE_SHOW" &&
-      <Space_Show 
-      spaceData={{...state.spaceData}}
-      popUp={state.popUp}
-      yesButtonFunc={() => reroute('/dashboard')}
-      noButtonFunc={() => togglePopUp(false)}
-      setVisualMode={setVisualMode}
-      reroute={reroute}
-      />
-  }
-  {state.visualMode === "REQUEST_FORM" &&
-    <RentalRequest user_id={props.user_id} space_id={state.spaceData.id} space={state.spaceData} setVisualMode={() => setVisualMode("SPACE_SHOW")} setPopUp={() => togglePopUp(true)}/>
-}
-
-        </article>
+      { state.visualMode === "SPACE_SHOW" &&
+        <Space_Show 
+        spaceData={{...state.spaceData}}
+        popUp={state.popUp}
+        yesButtonFunc={() => reroute('/dashboard')}
+        noButtonFunc={() => togglePopUp(false)}
+        setVisualMode={setVisualMode}
+        reroute={reroute}
+        />
+      }
+      { state.visualMode === "REQUEST_FORM" &&
+        <RentalRequest user_id={props.user_id} space_id={state.spaceData.id} space={state.spaceData} setVisualMode={() => setVisualMode("SPACE_SHOW")} setPopUp={() => togglePopUp(true)}/>
+      }
+    </article>
 
   )
 
