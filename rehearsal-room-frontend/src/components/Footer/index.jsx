6f4389f9204fa24aa2ec97dd9074495c258af752
Paul Ladd <vlendle@gmail.com>
2021-05-17 00:32:28 +0000

    Add github icon to footer w/ styling

--- rehearsal-room-frontend/src/components/Footer/index.jsx
+++ rehearsal-room-frontend/src/components/Footer/index.jsx
@@ -4,6 +4,13 @@ export default function Footer() {
       <div className="footer-section">
         <em>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></em>
       </div>
+      <div className="footer-section">
+        <em>
+          Created by Adele Noronha, Kate Davis, and Paul Ladd</em>
+        <a href="https://github.com/KateIsabelle/rehearsal-room">
+          <img src="GitHub-Mark-32px.png" alt="GitHub"/>
+        </a>
+      </div>
     </footer>
   )
 }
