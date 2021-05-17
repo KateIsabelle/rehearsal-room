b14efb8098e273c4e748b5d2f5bb612842413f1e
Paul Ladd <vlendle@gmail.com>
2021-05-17 16:11:03 +0000

    Clean up spacing in header

--- rehearsal-room-frontend/src/components/Header/index.jsx
+++ rehearsal-room-frontend/src/components/Header/index.jsx
@@ -10,31 +10,29 @@ import logo from './plans.svg'
 export default function Header(props) {
   let history = useHistory()
   return (
-
     <header>
     <div className="wrapper">
       <div>
-      <div className="header-list">
-        <NavLink to="/" component={Logo} />
-
-        <Link  to="/spaces/Vancouver" style={{textDecoration: 'none'}}><h3>Browse spaces</h3></Link>
-        <Link style={{textDecoration: 'none'}}><h3>About us</h3></Link>
-
+        <div className="header-list">
+          <NavLink to="/" component={Logo} />
+          <Link  to="/spaces/Vancouver" style={{textDecoration: 'none'}}><h3>Browse spaces</h3></Link>
+          <Link style={{textDecoration: 'none'}}><h3>About us</h3></Link>
         </div>
       </div>
       <div>
         {props.user ? (
-            <tag className="user-greeting-logout">
-                <Link to={'/dashboard'} style={{paddingRight: 13, textDecoration: 'none'}}><h3>{props.user.first_name}'s Profile</h3></Link>
-                <Button primary size="small" label="Log out" />
-            </tag>
+          <tag className="user-greeting-logout">
+            <Link to={'/dashboard'} style={{paddingRight: 13, textDecoration: 'none'}}>
+              <h3>{props.user.first_name}'s Profile</h3>
+            </Link>
+            <Button primary size="small" label="Log out" />
+          </tag>
           ) : (
             <>
               <Button primary onClick={()=>{history.push('/login')}}size="small" label="Log in" />
               <Button primary size="small" label="Sign up" />
             </>
           )}
-
       </div>
     </div>
   </header>
@@ -45,10 +43,10 @@ export default function Header(props) {
 function Logo() {
   return (
     <>
-    <Link to={'/'}><img src={logo} width="32" height="32" alt="" /></Link>
-    <h1>
-      Rehearsal Room
-    </h1>
+      <Link to={'/'}><img src={logo} width="32" height="32" alt="" /></Link>
+      <h1>
+        Rehearsal Room
+      </h1>
     </>
   )
 }
