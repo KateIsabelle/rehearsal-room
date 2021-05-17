e7aff7bd82d512a1f4a73d509055d24bb65ad4ea
KateIsabelle <kate.davis887@gmail.com>
2021-05-17 23:26:41 +0000

    rearrange elements in Space component

--- rehearsal-room-frontend/src/components/Space/Space_Show.jsx
+++ rehearsal-room-frontend/src/components/Space/Space_Show.jsx
@@ -30,49 +30,51 @@ export default function Space_Show(props) {
       }
 
       <div className="space-banner">
-        <div className="move-down">
+        <div className="">
           <div>
             <h1 className="mrg-med">{props.spaceData.title}</h1>
             <h3 className="mrg-med">{props.spaceData.city}</h3>
           </div>
-          <div className="space-photo-cont">
-            <img className="space-photo" src={props.spaceData.cover_photo_url} alt="property"></img>
-          </div>
         </div>
 
-        <div className="move-down">
+        <div className="">
           <div className="price-wrapper">
             <div className="mrg-med"><Button size="xlarge" label="Make a Request" onClick={() => props.setVisualMode("REQUEST_FORM")}/></div>
-            <div className="make-flex-col">
+            <div className="">
               <div>Price per day: ${props.spaceData.price_per_day / 100}</div>
               <div>Price per hour: ${props.spaceData.price_per_hour / 100}</div>
             </div>
           </div>
+        </div>
+      </div>
 
-          { false && <div className="map-container"><StaticMap className="" latitude={props.spaceData.latitude} longitude={props.spaceData.longitude}/></div> }
-          { true && <MapSection location={{address: props.spaceData.address, lat: props.spaceData.latitude, lng: props.spaceData.longitude}} zoomLevel={13} /> }
-          
-          { props.spaceData.organization_name && <div>Affiliated organization: {props.spaceData.organization_name}</div> }
+      <div className="lower-content">
+
+        <div className="space-photo-cont">
+          <img className="space-photo" src={props.spaceData.cover_photo_url} alt="property"></img>
+        </div>
+        <PhotoGrid />
+
+        { true && <MapSection location={{address: props.spaceData.address, lat: props.spaceData.latitude, lng: props.spaceData.longitude}} zoomLevel={13} /> }
+      
+        { props.spaceData.organization_name && <div>Affiliated organization: {props.spaceData.organization_name}</div> }
           <div>Contact: {props.spaceData.first_name} {props.spaceData.last_name}, {props.spaceData.email}</div>
+
+        <div className="space-info">
+          <div className="space-desc">
+            <p>{props.spaceData.description}</p>
+          </div>
+          <div className="space-features">
+            <h3>Features:</h3>
+            <AmenitiesList spaceData={props.spaceData}/>
+          </div>
         </div>
-      </div>
 
-      <PhotoGrid />
+        <OpeningHoursTable/>
 
-      <div className="space-info">
-        <div className="space-desc">
-          <p>{props.spaceData.description}</p>
+        <div className="browse-button"><Button size="large" label="Go Back to Listings" onClick={()=> props.reroute('/spaces/vancouver')}></Button></div>
+      
         </div>
-        <div className="space-features">
-          <h3>Features:</h3>
-          <AmenitiesList spaceData={props.spaceData}/>
-        </div>
-      </div>
-
-      <OpeningHoursTable/>
-
-      <div className="browse-button"><Button size="large" label="Go Back to Listings" onClick={()=> props.reroute('/spaces/vancouver')}></Button></div>
-    
     </Fragment>
   )
 }
