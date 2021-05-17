1d7df5bc8727e4a4839b0df2d327c2ad56363700
Adele Noronha <adele.c.noronha@gmail.com>
2021-05-17 05:42:02 +0000

    add comments

--- rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
+++ rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import './_Hero.scss'
 
+//build an array of images
+//loop through array of immages every 5000
+//
+
 export default function HeroV1(props) {
+  const imgArr = [
+    {},
+    {},
+    {}
+  ]
   return (
     <tag>
       <div id="container">
-      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/elijah-m-henderson-xgT3iQDIijU-unsplash_lmqcd5.jpg"/>
+      <img id="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/mostafa-meraji-Jby30ALhnms-unsplash_olrgbs.jpg"/>
       </div>
 
     </tag>
