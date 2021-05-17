41c1ae1b862b4ecdb17a6ef05a7f30b71d6ba6f7
Paul Ladd <vlendle@gmail.com>
2021-05-17 16:11:41 +0000

    Add alt message to hero image

--- rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
+++ rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
@@ -9,7 +9,7 @@ export default function HeroV1(props) {
       <div id="banner-container">
       {/* <img className="image" id="slide" sizes="100vw" src="
       https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1621230600/dance_mirror.jpg"/> */}
-      <img className="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/dramatic_dude.jpg" />
+      <img alt="Banner" className="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/dramatic_dude.jpg" />
       {/* <img className="image" sizes="100vw" src="https://res.cloudinary.com/dyo7gkw5s/image/upload/w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620877221/violin.jpg" /> */}
       </div>
     </tag>
