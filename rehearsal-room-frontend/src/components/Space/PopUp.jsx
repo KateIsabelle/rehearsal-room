9622f5b32ee7e0bb9cd70721faad99e1a37f01e7
KateIsabelle <kate.davis887@gmail.com>
2021-05-19 17:19:31 +0000

    make popup background blur

--- rehearsal-room-frontend/src/components/Space/PopUp.jsx
+++ rehearsal-room-frontend/src/components/Space/PopUp.jsx
@@ -3,26 +3,27 @@ import {Button} from '../Button/Button'
 export default function PopUp(props) {
   
   return (
-    <div className={props.className}>
-      <div className="pu-content">
-        <h3 className="pu-header pu">{props.header}</h3>
-        <p className=" pu-body pu">
-          {props.body}
-        </p>
-        <div className="pu">
-          {props.yesButton && 
-          <Button 
-            label={props.yesButton}
-            onClick={props.yesButtonFunc}
-          />}
-          {props.noButton && 
-          <Button 
-            label={props.noButton}
-            onClick={props.noButtonFunc}
-          />}
+    <div className="popup-cont">
+      <div className={props.className}>
+        <div className="pu-content">
+          <h3 className="pu-header pu">{props.header}</h3>
+          <p className=" pu-body pu">
+            {props.body}
+          </p>
+          <div className="pu">
+            {props.yesButton && 
+            <Button 
+              label={props.yesButton}
+              onClick={props.yesButtonFunc}
+            />}
+            {props.noButton && 
+            <Button 
+              label={props.noButton}
+              onClick={props.noButtonFunc}
+            />}
+          </div>
         </div>
       </div>
     </div>
-
   )
 }
