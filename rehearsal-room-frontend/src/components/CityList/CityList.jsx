8c3fed97f1693147c2745450af1f2fd393f2edf7
Adele Noronha <adele.c.noronha@gmail.com>
2021-05-19 18:30:25 +0000

    trial responsiveness

--- rehearsal-room-frontend/src/components/CityList/CityList.jsx
+++ rehearsal-room-frontend/src/components/CityList/CityList.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function CityList() {
   return (
     <>
-      <div className="container">
+      <div className="city-container">
         <h1>Pick an area to explore </h1>
         <div className="city-list">
           <Link to="/spaces/vancouver" style={{textDecoration: 'none'}}className="city-item">Vancouver</Link>
