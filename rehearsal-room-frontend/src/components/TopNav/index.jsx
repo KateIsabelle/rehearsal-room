ee8056a84276b179a76fe56ed9b682448d6cd3a3
KateIsabelle <kate.davis887@gmail.com>
2021-06-17 15:18:39 +0000

    add event listeners for hamburger menu click

--- rehearsal-room-frontend/src/components/TopNav/index.jsx
+++ rehearsal-room-frontend/src/components/TopNav/index.jsx
@@ -3,16 +3,34 @@ import { Button } from '../Button/Button';
 import { useHistory, NavLink, Link } from 'react-router-dom';
 import logo from '../Header/plans.svg'
 
-export default function HeaderNew() {
+const hamburgerMenu = (status) => {
+  const mainMenu = document.querySelector('.main-menu')
+
+  switch (status) {
+    case 'open':
+      // alert('open');
+      mainMenu.style.display = 'flex';
+      mainMenu.style.top = '0';
+      break;
+    case 'close':
+      // alert('close');
+      // mainMenu.style.display 
+      mainMenu.style.top = '-100%'
+      break;
+  }
+}
+
+
+export default function TopNav() {
   return (
     <nav>
-          <div className="logo"><NavLink to="/" component={Logo} /></div>
-      <div className="open-menu"><i class="fas fa-bars"></i></div>
+      <div className="logo"><NavLink to="/" component={Logo} /></div>
+      <div className="open-menu" onClick={() => hamburgerMenu('open')}><i class="fas fa-bars"></i></div>
       <ul className="main-menu">
         <li>Browse Spaces</li>
         <li>Login</li>
         <li>Sign Up</li>
-        <div className="close-menu"><i class="far fa-times-circle"></i></div>
+        <div className="close-menu" onClick={() => {hamburgerMenu('close')}}><i class="far fa-times-circle"></i></div>
       </ul>
     </nav>
   )
@@ -21,11 +39,10 @@ export default function HeaderNew() {
 
 function Logo() {
   return (
-    <>
-    <Link className="h-logo" to={'/'}><img height="42" width="42" src={logo} alt="" /></Link>
-    <h1>
-      | Rehearsal Room |
-    </h1>
-    </>
+    <Link className="header-logo" to={'/'}><img height="42" width="42" src={logo} alt="" />
+      <h1 className="">
+        | Rehearsal Room |
+      </h1>
+    </Link>
   )
 }
