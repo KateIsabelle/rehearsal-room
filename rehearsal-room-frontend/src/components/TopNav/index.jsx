30741a9568ba99136adcaee4466fb0c81c0546fd
KateIsabelle <kate.davis887@gmail.com>
2021-06-18 00:19:32 +0000

    toggle menu show on click

--- rehearsal-room-frontend/src/components/TopNav/index.jsx
+++ rehearsal-room-frontend/src/components/TopNav/index.jsx
@@ -1,36 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from '../Button/Button';
 import { useHistory, NavLink, Link } from 'react-router-dom';
 import logo from '../Header/plans.svg'
 
-const hamburgerMenu = (status) => {
-  const mainMenu = document.querySelector('.main-menu')
-
-  switch (status) {
-    case 'open':
-      // alert('open');
-      mainMenu.style.display = 'flex';
-      mainMenu.style.top = '0';
-      break;
-    case 'close':
-      // alert('close');
-      // mainMenu.style.display 
-      mainMenu.style.top = '-100%'
-      break;
-  }
-}
 
 
 export default function TopNav() {
+  const [menuState, setMenuState] = useState(false)
+
+  // const mainMenu = `main-menu ${menuState ? 'display-menu' : ''}`
+
+  const menuShow = menuState ?
+    {
+      display: 'flex',
+      top: 0
+    }
+    :
+    {
+      top: '-100%'
+    }
+
+
+  // useEffect(() => {
+  //   const hamburgerMenu = (status) => {
+  //     const mainMenu = document.querySelector('.main-menu')
+    
+  //     switch (status) {
+  //       case 'open':
+  //         mainMenu.style.display = 'flex';
+  //         mainMenu.style.top = '0';
+  //         break;
+  //       case 'close':
+  //         mainMenu.style.top = '-100%'
+  //         break;
+  //     }
+  //   }
+  // })
   return (
     <nav>
       <div className="logo"><NavLink to="/" component={Logo} /></div>
-      <div className="open-menu" onClick={() => hamburgerMenu('open')}><i class="fas fa-bars"></i></div>
-      <ul className="main-menu">
+      <div className="open-menu" onClick={() => setMenuState(true)}><i class="fas fa-bars"></i></div>
+      <ul className="main-menu" style={menuShow}>
         <li>Browse Spaces</li>
         <li>Login</li>
         <li>Sign Up</li>
-        <div className="close-menu" onClick={() => {hamburgerMenu('close')}}><i class="far fa-times-circle"></i></div>
+        <div className="close-menu" onClick={() => setMenuState(false)}><i class="far fa-times-circle"></i></div>
       </ul>
     </nav>
   )
